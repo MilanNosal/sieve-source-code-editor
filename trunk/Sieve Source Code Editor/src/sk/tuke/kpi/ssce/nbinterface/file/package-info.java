9a676ee4b84ce31c1a86cc4855364578a2c7b7e7
milan.nosal@gmail.com <milan.nosal@gmail.com@1561c54b-82b8-057f-a75c-6ee8b6e9394a>
2014-09-17 19:45:18 +0000

    removed obsolete data objects

--- trunk/Sieve Source Code Editor/src/sk/tuke/kpi/ssce/nbinterface/file/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-//@TemplateRegistration(folder = "Other", displayName = "#SieveJavatemplate_displayName", content = "SieveJavaTemplate.sj")
-//@Messages(value = "SieveJavatemplate_displayName=Empty SieveJava file")
-package sk.tuke.kpi.ssce.nbinterface.file;
